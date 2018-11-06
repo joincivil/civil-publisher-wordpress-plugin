98388b2f6fab1661b3cd489f3300202f500a903a
Toby Fox <tobyfox@gmail.com>
2018-11-06 15:09:10 +0100

    Fix post status on autosaved drafts

--- assets/post-panel/components/PostStatus.tsx
+++ assets/post-panel/components/PostStatus.tsx
@@ -38,7 +38,6 @@ class PostStatusComponent extends React.Component<PostStatusProps> {
           {this.props.published ? 're-publish by hitting the "Update" button' : "save again"} before continuing.
         </ErrorText>
       );
-      content = <p>Your post is published to your site and is ready to be published on the Civil network.</p>;
     } else if (this.props.published) {
       if (this.props.requirePublish) {
         content = <p>Your post is published to your site and is ready to be published on the Civil network.</p>;
@@ -111,21 +110,25 @@ export const PostStatus = withSelect(
       isCleanNewPost,
       isCurrentPostPublished,
       isSavingPost,
+      hasAutosave,
     } = selectStore("core/editor");
     const { isPluginDataMissing } = selectStore("civil/blockchain");
 
     const date = getEditedPostAttribute("date_gmt");
     const modifiedDate = getEditedPostAttribute("modified_gmt");
 
+    const pluginDataMissing = isPluginDataMissing();
+    const onlyAutosaved = pluginDataMissing && hasAutosave();
+
     return {
       requirePublish: ownProps.requirePublish,
-      saved: !isEditedPostDirty() && !isCleanNewPost(),
+      saved: !isEditedPostDirty() && !isCleanNewPost() && !onlyAutosaved,
       published: isCurrentPostPublished(),
       updated: modifiedDate && modifiedDate !== date,
       timestamp: modifiedDate || date,
       url: getEditedPostAttribute("link"),
       isSavingPost: isSavingPost(),
-      pluginDataMissing: isPluginDataMissing(),
+      pluginDataMissing,
     };
   },
 )(PostStatusComponent);
