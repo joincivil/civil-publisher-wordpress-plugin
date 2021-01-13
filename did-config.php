bbd2d3baee922aa5dcd01d6bbf3f298d26856f5a
Toby Fox <tobyfox@gmail.com>
2021-01-12 19:40:55 -0800

    fix: Escape output JSON in DID Configuration

--- did-config.php
+++ did-config.php
@@ -7,11 +7,12 @@
 
 namespace ConsenSys_VC_Publisher;
 
-$did_config = esc_js( get_option( DID_CONFIG_OPTION_KEY ) );
+$did_config = get_option( DID_CONFIG_OPTION_KEY );
 
 if ( empty( $did_config ) ) {
 	status_header( 500 );
 	echo '{"error":"DID configuration has not yet been initialized or there was an error initializing it."}';
 } else {
-	echo $did_config;
+	// Kind of dumb to decode then encode but seems like the only way to satisfy WordPress.Security.EscapeOutput.OutputNotEscaped - esc_js and esc_html etc. each mess up JSON in their own way.
+	echo wp_json_encode( json_decode( $did_config ), JSON_UNESCAPED_SLASHES );
 }
